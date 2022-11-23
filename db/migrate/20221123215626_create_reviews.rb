e9ca25bb99f986dca5b06f72c539977ada5b5da1
Jason Bracco <bracco.jason@gmail.com>
2022-11-23 17:04:03 -0500

    reviews migration populated

--- db/migrate/20221123215626_create_reviews.rb
+++ db/migrate/20221123215626_create_reviews.rb
@@ -1,7 +1,9 @@
 class CreateReviews < ActiveRecord::Migration[6.1]
   def change
     create_table :reviews do |t|
-
+      t.string :content
+      t.belongs_to :restaurant, null: false, foreign_key: true
+      t.belongs_to :user, null: false, foreign_key: true
       t.timestamps
     end
   end
