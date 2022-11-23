88f316bba6d1a89232943c287ca7f39ef953c8c1
Jason Bracco <bracco.jason@gmail.com>
2022-11-23 17:01:46 -0500

    user controller populated

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,18 @@
 class UsersController < ApplicationController
+    skip_before_action :authorize, only: :create
+
+    #post: signup
+    def create
+        user-User.create!(user_params)
+        session[:user_id] = user.id 
+        render json: user, status: :created
+    end
+
+    private
+
+    def user_params
+        params.permit(:username, :password, :password_confirmation, :bio, :age, :name, :image_url)
+    end
+
+
 end
