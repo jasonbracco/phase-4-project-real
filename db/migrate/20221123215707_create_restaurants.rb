15add77258c42e2a5cfa048fd96ecfd8933e9866
Jason Bracco <bracco.jason@gmail.com>
2022-11-23 17:04:48 -0500

    restaurant migration populated

--- db/migrate/20221123215707_create_restaurants.rb
+++ db/migrate/20221123215707_create_restaurants.rb
@@ -1,7 +1,10 @@
 class CreateRestaurants < ActiveRecord::Migration[6.1]
   def change
     create_table :restaurants do |t|
-
+      t.belongs_to :city, null: false, foreign_key: true
+      t.string :cuisine
+      t.string :name
+      t.ineteger :seats
       t.timestamps
     end
   end
