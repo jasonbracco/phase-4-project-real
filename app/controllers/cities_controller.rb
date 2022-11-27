6711c83ff6a8be21d550ea30343d53fa7daf0bc3
Jason Bracco <bracco.jason@gmail.com>
2022-11-27 16:18:55 -0500

    post route and controller action complete for cities

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,2 +1,13 @@
 class CitiesController < ApplicationController
+
+    def create
+        city=City.create!(city_params)
+        render json: city
+    end
+
+    private
+
+    def city_params
+        params.permit(:name)
+    end
 end
