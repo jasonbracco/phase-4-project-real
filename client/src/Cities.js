a07cf94f0b4118a0873ea67c9188f848cd92402a
Jason Bracco <bracco.jason@gmail.com>
2022-11-27 13:29:02 -0500

    adding age to profile page

---
+++ client/src/Cities.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Cities(){
+
+
+    return(
+
+        <div>
+            Cities will go here!
+        </div>
+    )
+}
+
+export default Cities
---
+++ client/src/UserProfile.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+function UserProfile({user}){
+
+
+    return(
+        <div>
+            Name: {user.name}
+            <br></br>
+            <br></br>
+            Age: {user.age}
+            <br></br>
+            <br></br>
+            Bio: {user.bio}
+
+        </div>
+    )
+}
+
+export default UserProfile
