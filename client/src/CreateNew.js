41746be30927c37c191c15247f9164b2aad8fd74
Jason Bracco <bracco.jason@gmail.com>
2022-11-27 14:35:29 -0500

    adding in all forms into createnew component

--- client/src/CreateNew.js
+++ client/src/CreateNew.js
@@ -1,36 +1,16 @@
 import React from "react"
+import CreateCity from './Forms/CreateCity'
+import CreateReview from './Forms/CreateReview'
+import CreateRestaurant from './Forms/CreateRestaurant'
 
 function CreateNew({user}){
 
 
     return(
         <div className="form-container">
-            <div className="city-form">
-                <form onSubmit={handleCitySubmit}>
-                    <h3>Submit a Review</h3>
-                    <input
-                        type="text"
-                        name="username"
-                        value={username}
-                        autoComplete="off"
-                        placeholder="Username"
-                        onChange={(e) => setUsername(e.target.value)}
-                    />
-                    <h3>Password:</h3>
-                    <input
-                        type="text"
-                        name="password"
-                        value={password}
-                        autoComplete="off"
-                        placeholder="Password"
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
-                    <br></br>
-                    <button type="submit">
-                        Sign In!
-                    </button>
-                </form>
-            </div>
+            <CreateCity />
+            <CreateReview />
+            <CreateRestaurant />
         </div>
     )
     
