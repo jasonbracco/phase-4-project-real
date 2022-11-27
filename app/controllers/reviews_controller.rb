fa6dc5acfa1f67bb226bc929cdbf1f7eeae1a32f
Jason Bracco <bracco.jason@gmail.com>
2022-11-27 16:16:14 -0500

    post route and controller action complete for reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,13 @@
 class ReviewsController < ApplicationController
+
+    def create
+        review=@current_user.reviews.create!(review_params)
+        render json: review, status: :created
+    end
+
+    private
+
+    def review_params
+        params.permit(:user_id, :restaurant_id, :content)
+    end
 end
