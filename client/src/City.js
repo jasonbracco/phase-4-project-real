c80c60a2ce95504a0e06d04754624898e364bd4a
Jason Bracco <bracco.jason@gmail.com>
2022-11-30 18:50:15 -0500

    setting up a handleClick for each city

--- client/src/City.js
+++ client/src/City.js
@@ -1,13 +1,17 @@
 import React from 'react'
 
 function City({city, restaurants, reviews}) {
+    
+    function handleClick(){
+        console.log("you clicked me")
+    }
 
     // This is where each city will be click-able, and it will show you all restaurants in each city.  
     //Need to pass in restaurants as props, then, onClick, display restaurants where city_id === city.id
 
     return(
         <div>
-        <div className="each-city">
+        <div className="each-city" onClick={handleClick}>
             {city.name}
         </div>
         </div>
