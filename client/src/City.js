7958e323af0b898ab4cb880ab34aedca16b9e274
Jason Bracco <bracco.jason@gmail.com>
2022-11-30 19:16:34 -0500

    can change each city entry into something else onClick

--- client/src/City.js
+++ client/src/City.js
@@ -1,20 +1,29 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './index.css'
 
 function City({city, restaurants, reviews}) {
 
-    function handleClick(){
-        console.log("you clicked me")
-    }
+    const [citySelected, setCitySelected] = useState(true)
+
+
 
     // This is where each city will be click-able, and it will show you all restaurants in each city.  
     //Need to pass in restaurants as props, then, onClick, display restaurants where city_id === city.id
 
     return(
         <div>
-        <div className="each-city" onClick={handleClick}>
-            {city.name}
-        </div>
+            {citySelected ? ( 
+                <div className="each-city" onClick={() => setCitySelected((citySelected) => !citySelected)}>
+                    {city.name}
+                </div>
+                ): 
+                (
+                <div onClick={() => setCitySelected((citySelected) => !citySelected)}>
+                    <p>Hi</p>
+                </div>
+                )}
+            {/* <break></break>
+            <break></break> */}
         </div>
     )
 }
