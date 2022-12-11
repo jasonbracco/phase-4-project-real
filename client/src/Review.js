a0ceb360226729fd0b4adb6e4981baf5cde3b0a7
Jason Bracco <bracco.jason@gmail.com>
2022-12-11 15:35:22 -0500

    cleaning up review component

--- client/src/Review.js
+++ client/src/Review.js
@@ -3,12 +3,9 @@ import React from 'react'
 function Review({review}){
 
     return(
-        <div>
-            {review.content}
-            <br></br>
-            {review.user_id}
-            <br></br>
-            {review.restaurant_id}
+        <div className="individual-review"> 
+            <p className="review-restaurant-name">{review.restaurant.name}</p>
+            <p className="review-content">{review.content}</p>
         </div>
     )
 }
