ef86a413e9b9d894cf2a2de30ddeacb72e384f08
Jason Bracco <bracco.jason@gmail.com>
2022-12-17 15:44:54 -0500

    adding in no restaurant logic

--- client/src/City.js
+++ client/src/City.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react' 
 import { useParams} from "react-router-dom";
 import Restaurant from './Restaurant'
 import './index.css'  
@@ -8,12 +8,22 @@ function City({restaurants, reviews}) {
     const {id} = useParams()
 
     const restaurantArray = restaurants.filter(restaurant => restaurant.city_id == id)
+    const howManyRestaurants = restaurantArray.length
 
     return(
         <div className="restaurant-review-containter">
-            {restaurantArray.map((restaurant) => {
-                return <Restaurant key={restaurant.id} restaurant={restaurant} reviews={reviews}/>
-            })}
+            {howManyRestaurants>0 ? (
+                <div>
+                    {restaurantArray.map((restaurant) => {
+                        return <Restaurant key={restaurant.id} restaurant={restaurant} reviews={reviews}/>
+                    })}
+                </div>
+            ) : (
+                <div className="no-restaurants">
+                    <p><em>No user has reviewed a restaurant in this city</em></p>
+                    <p><em>Go out and try one for yourself, and let us know how it is!</em></p>
+                </div>
+            )}
         </div>
     )
 }
